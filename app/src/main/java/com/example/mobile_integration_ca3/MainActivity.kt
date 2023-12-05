824d010f4959e332126795ae1cc17c4bcf398852
Cian Ashby <cianashby1337@gmail.com>
2023-12-05 07:39:00 +0000

    Add animations to list items

--- app/src/main/java/com/example/mobile_integration_ca3/MainActivity.kt
+++ app/src/main/java/com/example/mobile_integration_ca3/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.mobile_integration_ca3
 
 import Datasource
 import android.os.Bundle
+import androidx.compose.foundation.layout.Spacer
 import android.util.Log
+import androidx.compose.animation.animateContentSize
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -23,13 +28,22 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Button
+import androidx.compose.material3.IconButton
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.material.icons.filled.ExpandMore
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.ui.res.dimensionResource
 
 
 class MainActivity : ComponentActivity() {
@@ -65,24 +79,94 @@ fun DosesApp() {
 
 @Composable
 fun DoseCard(dose: Dose, modifier: Modifier = Modifier) {
+    var expanded by remember { mutableStateOf(false) }
     Card(modifier = modifier) {
-        Text(
-                text = LocalContext.current.getString(dose.stringResourceId),
-                modifier = Modifier.padding(16.dp),
-                style = MaterialTheme.typography.headlineSmall
-            )
+        Column(
+            modifier = Modifier
+                .animateContentSize(
+                    animationSpec = spring(
+                        dampingRatio = Spring.DampingRatioNoBouncy,
+                        stiffness = Spring.StiffnessMedium)
+                )
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(3.dp)
+            ) {
+                Text(
+                    text = LocalContext.current.getString(dose.datetime),
+                    modifier = Modifier.padding(16.dp),
+                    style = MaterialTheme.typography.headlineSmall
+                )
+                Spacer(modifier = Modifier.weight(1f))
+                DoseCardButton(
+                    expanded = expanded,
+                    onClick = { expanded = !expanded  }
+
+                )
+            }
+            if (expanded) {
+                DoseContents(
+                    dose.contents,
+                    modifier = Modifier.padding(
+                        start = 5.dp,
+                        top = 3.dp,
+                        end = 5.dp,
+                        bottom = 5.dp
+                    )
+                )
+            }
+        }
     }
-    Log.d("DoseCard","DoseCard successfully loaded with the string:" + LocalContext.current.getString(dose.stringResourceId))
+    Log.d("DoseCard","DoseCard successfully loaded with the string:" + LocalContext.current.getString(dose.datetime))
     // Looking at Logcat, we observe that two list items above/below the currently
     // visible list items are loaded. This is the result of using a LazyColumn, which
     // renders a more narrow range of items that need to be visible. Performance benefit
+}
+
+@Composable
+private fun DoseCardButton(
+    expanded: Boolean,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    IconButton(
+        onClick = onClick,
+        modifier = modifier
+    ) {
+        Icon(
+            imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+            contentDescription = stringResource(R.string.expand_button_content_description),
+            tint = MaterialTheme.colorScheme.secondary
+        )
+    }
+}
+
+@Composable
+fun DoseContents(
+    @StringRes contents: Int,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+    ) {
+        Text(
+            text = "Contents:",
+            style = MaterialTheme.typography.labelSmall
+        )
+        Text(
+            text = stringResource(contents),
+            style = MaterialTheme.typography.bodyLarge
+        )
+    }
 }
 
 @Preview
 @Composable
 private fun DoseCardPreview() {
     Mobile_Integration_CA3Theme(darkTheme = true) {
-        DoseCard(Dose(R.string.Dose1))
+        DoseCard(Dose(R.string.Dose1,R.string.Contents1))
     }
 }
 
--- app/src/main/java/com/example/mobile_integration_ca3/model/Dose.kt
+++ app/src/main/java/com/example/mobile_integration_ca3/model/Dose.kt
@@ -2,5 +2,6 @@ package com.example.mobile_integration_ca3.model
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 data class Dose(
-    @StringRes val stringResourceId: Int
+    @StringRes val datetime: Int,
+    @StringRes val contents: Int
 )
